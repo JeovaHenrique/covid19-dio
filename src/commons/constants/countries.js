6cfd56d603dd30f3b369c55540309e9a21d7a352
JeovaHenrique <jeovahenrique92@gmail.com>
2022-05-27 18:55:51 -0300

    projeto covid19

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,35 @@
+import BrazilFlag from '../../assets/img/brasil.png'
+import ChinaFlag from '../../assets/img/china.png'
+import GermanyFlag from '../../assets/img/alemanha.png'
+import ItalyFlag from '../../assets/img/italia.png'
+import UsaFlag from '../../assets/img/eua.png'
+
+const COUNTRIES = [
+  {
+    label: 'Alemanha',
+    value: 'germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'China',
+    value: 'china',
+    flag: ChinaFlag
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+    flag: UsaFlag
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+    flag: ItalyFlag
+  }
+]
+
+export default COUNTRIES
